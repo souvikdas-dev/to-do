1c59a0a0836c55397914601b1a8ec9abbeae2dc1
Souvik Das <sd8338820@gmail.com>
2024-01-10 22:11:33 +0530

    Rename Task model to ToDo

--- app/Models/ToDo.php
+++ app/Models/ToDo.php
@@ -5,9 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Task extends Model
+class ToDo extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'todos';
 
     /**
      * The attributes that are mass assignable.
