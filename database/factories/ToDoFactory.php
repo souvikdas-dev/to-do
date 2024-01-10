d1e1d2550ea45a25850b1c1c1fb8cab0733fa543
Souvik Das <sd8338820@gmail.com>
2024-01-10 22:13:11 +0530

    Rename TaskFactory to ToDoFactory

--- database/factories/ToDoFactory.php
+++ database/factories/ToDoFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ToDo>
  */
-class TaskFactory extends Factory
+class ToDoFactory extends Factory
 {
     /**
      * Define the model's default state.
