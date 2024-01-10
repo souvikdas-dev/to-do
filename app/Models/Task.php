5040d0939ec1e08d4ec94aa86b6f1e0f865c9c1b
Souvik Das <sd8338820@gmail.com>
2024-01-10 21:21:39 +0530

    Define $fillable, $casts, $attributes to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'task',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_completed' => 'boolean',
+    ];
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'is_completed' => false,
+    ];
 }
